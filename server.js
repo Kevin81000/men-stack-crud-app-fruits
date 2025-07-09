5d33cda20c02a15f56bb98a0ab828f482ac7941b
Kevin8100 <Kevin.tyler8100@gmail.com>
2025-07-09 10:04:09 -0500

    1st commit

--- server.js
+++ server.js
@@ -3,8 +3,14 @@
 const dotenv = require("dotenv"); // require package
 dotenv.config(); // Loads the environment variables from .env file
 const express = require("express");
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+console.log("MongoDB URI:", process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");
@@ -12,7 +18,6 @@ app.get("/", async (req, res) => {
 app.listen(3000, () => {
     console.log ("Listening on port 3000")
 });
-
 
 
 
