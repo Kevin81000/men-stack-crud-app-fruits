f96427b8b261dc1de7c36ba8d33d301f17b6561d
Kevin8100 <Kevin.tyler8100@gmail.com>
2025-07-11 05:02:43 -0500

    1st

---
+++ models/Fruit.js
@@ -0,0 +1,12 @@
+ // models/fruit.js
+
+const mongoose = require('mongoose');
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  isReadyToEat: Boolean,
+});
+const Fruit = mongoose.model("Fruit", fruitSchema); 
+// models/fruit.js
+
+module.exports = Fruit;
--- server.js
+++ server.js
@@ -8,16 +8,45 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 console.log("MongoDB URI:", process.env.MONGODB_URI);
+
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+const Fruit = require("./models/fruit.js");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+  });
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
+});
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find ();
+  console.log(allFruits); //log the fruits!
+  res.send("Welcome to the index page!");
+});
+
+
 app.listen(3000, () => {
     console.log ("Listening on port 3000")
 });
+
+
+
 
 
 
